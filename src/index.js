9f5f416f0a0f857412997edd8c765cd24dbb2311
moisestech <m@moises.tech>
2021-02-03 07:15:48 -0500

    steps to complete for app and es6 test

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Install dependencies
+// Import dependencies
+// Setup webcam and canvas
+// Define references to those
+// Load facemesh
+// Detect function
+// Drawing utilities
+// Load triangulation
+// Setup triangle path
+// Setup point drawing
+// Add drawMesh to detect function
+
+let project_name = "Tensorflow.js Face Recognition";
+
+console.log({project_name});
