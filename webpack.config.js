ce169717e9a9492fa6814a4683e3bada152a3dfb
moisestech <m@moises.tech>
2021-02-03 08:31:07 -0500

    output filename path, mode dev, module rules test loader options

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,37 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  path: path.resolve(__dirname, 'dist')
+  output: {
+    filename:'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test:/\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test:/\.mp4/,
+        loader:'file-loader',
+        options: {
+          name:'[name].[ext]',
+          outputPath:'video/'
+        }
+      },
+      {
+        test:/\.(jpg|jpeg|png|gif)$/,
+        loader:'file-loader',
+        options: {
+          name:'[name].[ext]',
+          outputPath:'image/'
+        }
+      },
+      {
+        test:/\.ico$/,
+        loader:'file-loader'
+      }
+    ]
+  }
 }
