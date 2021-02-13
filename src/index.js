4899735ee1da8d6bbbcb0d79219a934fb9f808ba
moisestech <m@moises.tech>
2021-02-13 13:24:28 -0500

    init react reac-dom tensorflow and tfjs-face-model

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@
 // Setup point drawing
 // Add drawMesh to detect function
 
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+// OLD MODEL
+//import * as facemesh from "@tensorflow-models/facemesh";
+
+// NEW MODEL
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import Webcam from "react-webcam";
+import { drawMesh } from "./utilities";
+
 let project_name = "Tensorflow.js Face Recognition";
 
 console.log({project_name});
