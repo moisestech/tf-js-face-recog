31597d6522d7a80bbeab18522174243cb307c352
moisestech <m@moises.tech>
2021-02-18 06:29:08 -0500

    init utils const TRIANGULATION, drawPath, drawMesh

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+// Triangulations sets of three
+export const TRIANGULATION = [];
+
+// Triangle drawing method
+const drawPath = () => {
+  return null;
+}
+
+// Drawing Mesh
+export const drawMesh = () => {
+  return null;
+}
