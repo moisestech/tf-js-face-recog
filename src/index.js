1462e16b78d24ba54c6bd605482b025b89189bfc
moisestech <m@moises.tech>
2021-02-18 06:25:30 -0500

    react react-dom, imp func comp App, StrictMode

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-// Install dependencies
-// Import dependencies
-// Setup webcam and canvas
-// Define references to those
-// Load facemesh
-// Detect function
-// Drawing utilities
-// Load triangulation
-// Setup triangle path
-// Setup point drawing
-// Add drawMesh to detect function
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
 
-import React, { useRef, useEffect } from "react";
-import "./App.css";
-import * as tf from "@tensorflow/tfjs";
-// OLD MODEL
-//import * as facemesh from "@tensorflow-models/facemesh";
-
-// NEW MODEL
-import * as facemesh from "@tensorflow-models/face-landmarks-detection";
-import Webcam from "react-webcam";
-import { drawMesh } from "./utilities";
-
-// https://github.com/nicknochnack/FacialLandmarkDetection
-
-let project_name = "Tensorflow.js Face Recognition";
-
-console.log({project_name});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
