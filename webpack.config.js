a3fc9de7c7c6901074ed40317307ffaf20d26e5d
moisestech <m@moises.tech>
2021-02-19 06:39:50 -0500

    babel-loader preset react

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets:['@babel/preset-env'],
+            presets:[
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
             plugins: [
               '@babel/plugin-transform-runtime',
               ['@babel/plugin-proposal-pipeline-operator', {proposal:'minimal'}],
