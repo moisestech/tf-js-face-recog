831e16e11d0d7116c89ba191f281053f225538df
moisestech <m@moises.tech>
2021-03-04 06:43:27 -0500

    setup drawing utility, drawMesh()

--- src/utils/index.js
+++ src/utils/index.js
@@ -2658,6 +2658,19 @@ const drawPath = (ctx, points, closePath) => {
 }
 
 // Point mesh drawing method
-export const drawMesh = () => {
-  return null;
+export const drawMesh = (predictions, ctx) => {
+  if (predictions.length > 0) {
+    predictions.forEach(prediction => {
+      const keypoints = prediction.scaledMesh;
+      for (let i = 0; i < keypoints.length; i++) {
+        const x = keypoints[i][0];
+        const y = keypoints[i][1];
+
+        ctx.beginPath();
+        ctx.arc(x, y, 1, 0, 3 * Math.PI);
+        ctx.fillStyle = "aqua";
+        ctx.fill();
+      }
+    })
+  }
 }
